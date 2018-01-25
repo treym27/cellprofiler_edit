4a5eaf1a981c6bb8666645dec32ce0bd4191bc41
Steven Remington <stevenrremington@gmail.com>
2018-01-25 14:21:25 -0500

    Adding util and test files, most are placeholders for proof of build file

---
+++ src/main/java/fileutil/CSVUtil.java
@@ -0,0 +1,16 @@
+package fileutil;
+
+import org.apache.commons.csv.CSVParser;
+
+
+public class CSVUtil{
+    private String fileName; 
+    public CSVUtil(String fileName){
+        this.fileName = fileName;
+    }
+
+
+    public int retOne(){
+        return 1;
+    }
+}
