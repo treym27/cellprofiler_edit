b565855167832beeeafcc12dec0786bded00ff3b
Skyler Burgoyne <skyler.burgoyne13@yahoo.com>
2018-01-30 12:27:48 -0500

    Change directories to github repo

--- src/main/java/source/Entry.java
+++ src/main/java/source/Entry.java
@@ -8,8 +8,8 @@ public class Entry{
     	try {
     		FileCopyUtil getImagePath = new FileCopyUtil("C:/Users/Skyler/Documents/TestImages/TestImages/Batch/Buyout");
     		String imagePath = getImagePath.condenseDirectory();
-        Process process = Runtime.getRuntime().exec("\"C:/Program Files (x86)/CellProfiler/CellProfiler.exe\" -c -r -i " + imagePath + " -o C:/Users/Skyler/Documents -p C:/Users/Skyler/Documents/Version3/Config.cppipe");
-
+        Process process = Runtime.getRuntime().exec("\"C:/Program Files (x86)/CellProfiler/CellProfiler.exe\" -c -r -i " + imagePath + " -o C:/Users/Skyler/Documents/CellProfilerWrapper/CellProfilerWrapper -p C:/Users/Skyler/Documents/CellProfilerWrapper/CellProfilerWrapper/Pipelines/Configuration/Config.cppipe");
+        
    		}
    		catch(IOException e) {
    			System.out.println(e);
